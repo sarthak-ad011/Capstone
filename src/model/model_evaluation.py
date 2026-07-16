99d47c2e34afe15720ed2454980780c353861d2c
Sarthak <sarthakanand1409@gmail.com>
2026-07-16 05:33:27 +0530

    refactor: implement conditional DagsHub authentication to support both local and CI environments

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -10,30 +10,17 @@ import dagshub
 import os
 from src.logger import logging
 
-
-# Below code block is for production use
-# -------------------------------------------------------------------------------------
-# Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
-
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
-
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
-
-# # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
-# -------------------------------------------------------------------------------------
-
-# Below code block is for local use
-# -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri('https://dagshub.com/sarthak-ad011/Capstone.mlflow')
-dagshub.init(repo_owner='sarthak-ad011', repo_name='Capstone', mlflow=True)
-# -------------------------------------------------------------------------------------
+# Auto-detect CI vs local authentication
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if dagshub_token:
+    # Production/CI: use token-based auth
+    os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+    mlflow.set_tracking_uri('https://dagshub.com/sarthak-ad011/Capstone.mlflow')
+else:
+    # Local: use interactive dagshub auth
+    mlflow.set_tracking_uri('https://dagshub.com/sarthak-ad011/Capstone.mlflow')
+    dagshub.init(repo_owner='sarthak-ad011', repo_name='Capstone', mlflow=True)
 
 
 def load_model(file_path: str):
--- src/model/register_model.py
+++ src/model/register_model.py
@@ -11,29 +11,17 @@ import warnings
 warnings.simplefilter("ignore", UserWarning)
 warnings.filterwarnings("ignore")
 
-# Below code block is for production use
-# -------------------------------------------------------------------------------------
-# Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
-
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
-
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
-# # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
-# -------------------------------------------------------------------------------------
-
-
-# Below code block is for local use
-# -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri('https://dagshub.com/sarthak-ad011/Capstone.mlflow')
-dagshub.init(repo_owner='sarthak-ad011', repo_name='Capstone', mlflow=True)
-# -------------------------------------------------------------------------------------
+# Auto-detect CI vs local authentication
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if dagshub_token:
+    # Production/CI: use token-based auth
+    os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+    mlflow.set_tracking_uri('https://dagshub.com/sarthak-ad011/Capstone.mlflow')
+else:
+    # Local: use interactive dagshub auth
+    mlflow.set_tracking_uri('https://dagshub.com/sarthak-ad011/Capstone.mlflow')
+    dagshub.init(repo_owner='sarthak-ad011', repo_name='Capstone', mlflow=True)
 
 
 def load_model_info(file_path: str) -> dict:
